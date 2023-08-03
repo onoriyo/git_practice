bca6fb990af1a247eca34babccf1a9aedd9b411e
onoriyo <m2230080@photon.chitose.ac.jp>
2023-08-04 01:01:02 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+def func1():
+    print('func1 in hello.py')
+
+def func2():
+    print('func2 in hello.py')
+
+def func3():
+    print('func3 in func2 in hello.py')
+
+def main():
+    func1()
+    func2()
+    func3()
+    
